55acc3cddd438ca8fe87b806d0c7399c9776b611
Łukasz Kolber <69080157+psp515@users.noreply.github.com>
2024-01-04 21:49:49 +0100

    feat(setup docker and serilog)

--- src/ImageHub.Api/Program.cs
+++ src/ImageHub.Api/Program.cs
@@ -1,13 +1,17 @@
+using Serilog;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Host.UseSerilog((context, configuration) => 
+    configuration.ReadFrom.Configuration(context.Configuration));
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
