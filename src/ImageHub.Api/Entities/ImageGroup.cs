9346d6e24fd9254c2efb9eb21c14b47f7e0d9140
Łukasz Kolber <69080157+psp515@users.noreply.github.com>
2024-01-06 15:59:15 +0100

    feat(entities)

---
+++ src/ImageHub.Api/Entities/ImageGroup.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ImageHub.Api.Entities;
+
+public class ImageGroup
+{
+    [Key]
+    public Guid Id { get; set; }
+
+    public string Name { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+
+
+    public IList<Image> Images { get; set; } = new List<Image>();
+
+    public DateTime CreatedOnUtc { get; set; } = DateTime.UtcNow;
+    public DateTime EditedAtUtc { get; set; } = DateTime.UtcNow;
+}
---
+++ src/ImageHub.Api/Entities/Thumbnail.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ImageHub.Api.Entities;
+
+public class Thumbnail
+{
+    [Key]
+    public Guid Id { get; set; }
+
+    public string Name { get; set; } = string.Empty;
+    public byte[] Bytes { get; set; } = Array.Empty<byte>();
+    public string Description { get; set; } = string.Empty;
+    public string FileExtension { get; set; } = string.Empty;
+    public decimal Size { get; set; }
+
+    public Image Image { get; set; } = default!;
+
+    public DateTime CreatedOnUtc { get; set; } = DateTime.UtcNow;
+    public DateTime EditedAtUtc { get; set; } = DateTime.UtcNow;
+}
