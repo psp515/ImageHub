a04d3d66f91a6331e9105f49f1e0bf3beef54221
Łukasz Kolber <69080157+psp515@users.noreply.github.com>
2024-01-24 22:02:23 +0100

    feat(Image Packs tests extensions)

---
+++ src/ImageHub.Api.Tests/Features/ImagePack/GetImagePacksIntegrationTests.cs
@@ -0,0 +1,33 @@
+﻿using ImageHub.Api.Tests.Features.ImagePack.Models;
+using System.Net;
+
+namespace ImageHub.Api.Tests.Features.ImagePack;
+
+public class GetImagePacksIntegrationTests(IntegrationTestWebAppFactory factory) : BaseImagePackIntegrationTests(factory)
+{
+    [Fact]
+    public async Task GetImagePacks()
+    {
+        //Arrange
+        var name1 = $"Test {Guid.NewGuid()}";
+        var name2 = $"Test {Guid.NewGuid()}";
+
+        //Act   
+        var response1 = await AddImagePackWithData(name1);
+        var response2 = await AddImagePackWithData(name2);
+
+        var getResponse = await _client.GetAsync($"/api/imagepacks");
+
+        var packs = await TestsCommon.Deserialize<ImagePacksDto>(getResponse);
+
+        //Assert
+        Assert.Equal(HttpStatusCode.Created, response1.StatusCode);
+        Assert.Equal(HttpStatusCode.Created, response2.StatusCode);
+
+        Assert.Equal(HttpStatusCode.OK, getResponse.StatusCode);
+        Assert.True(packs!.Items.Count >= 2);
+
+        Assert.Contains(packs.Items, x => x.Name == name1);
+        Assert.Contains(packs.Items, x => x.Name == name2);
+    }
+}
---
+++ src/ImageHub.Api.Tests/Features/ImagePack/UpdateImagePackIntegrationTests.cs
@@ -0,0 +1,56 @@
+﻿using ImageHub.Api.Contracts.ImagePacks.UpdateImagePack;
+using ImageHub.Api.Tests.Features.ImagePack.Models;
+using ImageHub.Api.Tests.Shared.Responses;
+using System.Net;
+
+namespace ImageHub.Api.Tests.Features.ImagePack;
+
+public class UpdateImagePackIntegrationTests(IntegrationTestWebAppFactory factory) : BaseImagePackIntegrationTests(factory)
+{
+    [Fact]
+    public async Task UpdateImagePack()
+    {
+        //Arrange
+        var name = $"Test {Guid.NewGuid()}";
+
+        var updateRequest = new UpdateImagePackRequest()
+        {
+            Description = "Updated Test Image Pack Description."
+        };
+        var updateContent = TestsCommon.Serialize(updateRequest);
+
+        //Act
+        var response = await AddImagePackWithData(name);
+        var idObject = await TestsCommon.Deserialize<IdResponse>(response);
+        var updateResponse = await _client.PatchAsync($"/api/imagepacks/{idObject.Id}", updateContent);
+        var getResponse = await _client.GetAsync($"/api/imagepacks/{idObject!.Id}");
+        var pack = await TestsCommon.Deserialize<ImagePackDto>(getResponse);
+
+        //Assert
+        Assert.Equal(HttpStatusCode.Created, response.StatusCode);
+        Assert.Equal(HttpStatusCode.NoContent, updateResponse.StatusCode);
+        Assert.Equal(HttpStatusCode.OK, getResponse.StatusCode);
+        Assert.Equal(name, pack.Name);
+        Assert.Equal(updateRequest.Description, pack.Description);
+    }
+
+    [Fact]
+    public async Task UpdateUnexistingImagePackFails()
+    {
+        //Arrange
+        var updateRequest = new UpdateImagePackRequest()
+        {
+            Description = "Updated Test Image Pack Description."
+        };
+        var updateContent = TestsCommon.Serialize(updateRequest);
+
+        //Act
+        var response = await AddImagePackWithData();
+
+        var updateResponse = await _client.PatchAsync($"/api/imagepacks/{Guid.NewGuid()}", updateContent);
+
+        //Assert
+        Assert.Equal(HttpStatusCode.Created, response.StatusCode);
+        Assert.Equal(HttpStatusCode.NotFound, updateResponse.StatusCode);
+    }
+}
